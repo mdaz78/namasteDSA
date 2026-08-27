a84d249362533a8506b503b59b6d94cf6f6771c4
Md Abu Zafar <mdabuzafar78@gmail.com>
2026-08-27 20:48:58 +0530

    feat: solve some patterns problems

---
+++ warmUp/starPatterns.js
@@ -0,0 +1,126 @@
+/**
+ * For n = 4
+ * "  * * * *
+ * "  * * * *
+ * "  * * * *
+ * "  * * * *
+ */
+
+const pattern1 = (n) => {
+  for (let i = 0; i < n; i++) {
+    let row = "";
+
+    for (let j = 0; j < n; j++) {
+      row += "* ";
+    }
+
+    console.log(row);
+  }
+};
+
+console.log("#############");
+pattern1(4);
+console.log("#############");
+
+/**
+ *
+ * for n = 4
+ *
+ * ```
+ * *
+ * **
+ * ***
+ * ****
+ * ```
+ *
+ */
+
+const pattern2 = (n) => {
+  for (let i = 0; i < n; i++) {
+    let row = "";
+
+    for (let j = 0; j <= i; j++) {
+      row += "* ";
+    }
+
+    console.log(row);
+  }
+};
+
+console.log("#############");
+pattern2(4);
+console.log("#############");
+
+/**
+ * n = 5
+ *
+ * 1
+ * 1 2
+ * 1 2 3
+ * 1 2 3 4
+ * 1 2 3 4 5
+ */
+const pattern3 = (n) => {
+  for (let i = 1; i <= n; i++) {
+    let row = [];
+
+    for (let j = 1; j <= i; j++) {
+      row.push(j);
+    }
+
+    console.log(row.join(" "));
+  }
+};
+
+console.log("#############");
+pattern3(5);
+console.log("#############");
+
+/**
+ * n = 5
+ *
+ * 1
+ * 2 2
+ * 3 3 3
+ * 4 4 4 4
+ * 5 5 5 5 5
+ */
+const pattern4 = (n) => {
+  for (let i = 1; i <= n; i++) {
+    let rows = "";
+
+    for (let j = 1; j <= i; j++) {
+      rows += `${i} `;
+    }
+
+    console.log(rows);
+  }
+};
+
+console.log("#############");
+pattern4(5);
+console.log("#############");
+
+/**
+ * n = 5
+ * 1 2 3 4 5
+ * 1 2 3 4
+ * 1 2 3
+ * 1 2
+ * 1
+ */
+const pattern5 = (n) => {
+  for (let i = 0; i < n; i++) {
+    let rows = "";
+
+    for (let j = 0; j < n - i; j++) {
+      rows += `${j + 1} `;
+    }
+
+    console.log(rows);
+  }
+};
+
+console.log("#############");
+pattern5(5);
+console.log("#############");
